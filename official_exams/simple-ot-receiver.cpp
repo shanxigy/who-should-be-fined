9a8a7436cdd72a87b1697c1b7b15449f09b8857e
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2019-04-12 14:10:52 -0700

    add simple ot sender/receiver

---
+++ official_exams/simple-ot-receiver.cpp
@@ -0,0 +1,66 @@
+#include <ot/simpleot.h>
+#include <ot/OTconstants.h>
+#include <ENCRYPTO_utils/channel.h>
+#include <ENCRYPTO_utils/thread.h>
+#include <ENCRYPTO_utils/connection.h>
+#include <ENCRYPTO_utils/sndthread.h>
+#include <ENCRYPTO_utils/rcvthread.h>
+#include <ENCRYPTO_utils/cbitvector.h>
+#include <ENCRYPTO_utils/socket.h>
+#include <iostream>
+
+static const char* m_cConstSeed[2] = {"437398417012387813714564100", "15657566154164561"};
+
+int
+main(int argc, char** argv)
+{
+	std::string address = "127.0.0.1";
+	uint16_t port = 7766;
+	uint32_t nsndvals = 2;
+	uint32_t m_nSecParam = 128;
+	auto m_eFType = ECC_FIELD;
+
+	crypto *m_cCrypt = new crypto(m_nSecParam, (uint8_t*)m_cConstSeed[1]);
+  CLock *glock = new CLock();
+
+	std::unique_ptr<CSocket> m_Socket = Connect(address, port);
+	if (!m_Socket) {
+		std::cerr << "Connect failed on " << address << ":" << port << "\n";
+		std::exit(1);
+	}
+
+	auto sndthread = new SndThread(m_Socket.get(), glock);
+	auto rcvthread = new RcvThread(m_Socket.get(), glock);
+
+	rcvthread->Start();
+	sndthread->Start();
+
+	int64_t m_nBaseOTs = 1000;
+	auto m_cBaseOT = new SimpleOT(m_cCrypt, m_eFType);
+	channel* chan = new channel(OT_ADMIN_CHANNEL, rcvthread, sndthread);
+	uint8_t* pBuf = (uint8_t*) malloc(m_cCrypt->get_hash_bytes() * m_nBaseOTs * nsndvals);
+
+	CBitVector* U = new CBitVector();
+	U->Create(m_nBaseOTs, m_cCrypt);
+	for (uint32_t i = 0; i < m_nBaseOTs; i++)
+		U->SetBit(i, 0);
+
+#ifdef OTTiming
+	timespec np_begin, np_end;
+	clock_gettime(CLOCK_MONOTONIC, &np_begin);
+#endif
+
+		m_cBaseOT->Receiver(nsndvals, m_nBaseOTs, U, chan, pBuf);
+		std::cout << "endddd" << std::endl;
+
+#ifdef OTTiming
+	clock_gettime(CLOCK_MONOTONIC, &np_end);
+	printf("Time for performing the base-OTs: %f seconds\n", getMillies(np_begin, np_end));
+#endif
+
+	free(pBuf);
+	chan->synchronize_end();
+	delete(chan);
+	delete crypt;
+  delete glock;
+}
